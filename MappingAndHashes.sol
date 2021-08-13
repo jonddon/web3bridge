2b972d061208a32c7f2f1c1af7c0b7f9d5dccf93
John Odey <37102658+jonddon@users.noreply.github.com>
2021-08-13 08:24:45 +0100

    Create MappingAndHashes.sol

---
+++ MappingAndHashes.sol
@@ -0,0 +1,33 @@
+// A contract contains two very important mappings which are used to track some very *private* info...
+// The first mapping allows you to know how many times an address has called a function, 
+// the second one allows you to know the function hash that the address called last...
+// Write three different functions that don't *really do anything but returns a bool value* 
+// and update the mappings accordingly
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract mappingsAssignment{
+    mapping(address => uint) public addressToInt;
+    mapping(address => bytes32) public addressToHash;
+    
+    function first() public returns(bool){
+        addressToInt[msg.sender]++;
+        addressToHash[msg.sender] = keccak256(abi.encodeWithSignature("first()"));
+        return true;
+    }
+    
+    
+     function second() public returns(bool){
+         addressToInt[msg.sender]++;
+         addressToHash[msg.sender] = keccak256(abi.encodeWithSignature("second()"));
+        return true;
+    }
+    
+     function third() public returns(bool){
+         addressToInt[msg.sender]++;
+         addressToHash[msg.sender] = keccak256(abi.encodeWithSignature("third()"));
+        return true;
+    }
+    
+}
